2c18ab75435a2b745156e6904b1d3dd32bc8e488
Megan Silva <megansilva@Megans-MacBook-Pro.local>
2022-08-14 16:30:33 -0400

    adding thought route

---
+++ routes/api/ThoughtRoute.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+
+const {
+    getThought,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction,
+    deleteThought
+} = require('../../controllers/thoughtController');
+
+router.route("/").get(getThought).post(createThought);
+
+router.route('/:thoughtsId')
+.get(getSingleThought)
+.put(updateThought)
+.delete(deleteThought);
+
+router.route('/:thoughtId/reactions')
+.post(createReaction);
+
+router.route('/:thoughtId/reactions/:reactionId')
+.delete(deleteReaction);
+
+module.exports = router;
